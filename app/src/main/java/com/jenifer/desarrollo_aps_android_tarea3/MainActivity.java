b31bd0897e369eaec8713e019dd40c33636221b6
eni86 <61356060+eni86@users.noreply.github.com>
2020-10-04 09:27:29 -0300

    .

--- app/src/main/java/com/jenifer/desarrollo_aps_android_tarea3/MainActivity.java
+++ app/src/main/java/com/jenifer/desarrollo_aps_android_tarea3/MainActivity.java
@@ -51,7 +51,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toolbar miActionBar = (Toolbar) findViewById(R.id.actionBar);
+        setSupportActionBar(miActionBar);
         listaMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
 
         LinearLayoutManager llm = new LinearLayoutManager(this);
