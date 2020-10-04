3d3ebec00408a4774e29edc5ffc083a8801f39b6
eni86 <61356060+eni86@users.noreply.github.com>
2020-10-04 09:13:51 -0300

    .

---
+++ app/src/main/java/com/jenifer/desarrollo_aps_android_tarea3/Lista5Mascotas.java
@@ -0,0 +1,14 @@
+package com.jenifer.desarrollo_aps_android_tarea3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lista5Mascotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista5_mascotas);
+    }
+}
