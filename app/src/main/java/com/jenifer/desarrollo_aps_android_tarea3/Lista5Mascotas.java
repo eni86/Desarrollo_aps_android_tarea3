dc2f78f79796e9e2727c3750de50d554ca59f40a
eni86 <61356060+eni86@users.noreply.github.com>
2020-10-05 10:04:12 -0300

    Update Lista5Mascotas.java

--- app/src/main/java/com/jenifer/desarrollo_aps_android_tarea3/Lista5Mascotas.java
+++ app/src/main/java/com/jenifer/desarrollo_aps_android_tarea3/Lista5Mascotas.java
@@ -1,6 +1,8 @@
 package com.jenifer.desarrollo_aps_android_tarea3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toolbar;
@@ -12,6 +14,20 @@ public class Lista5Mascotas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista5_mascotas);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        listaMascotas = (RecyclerView) findViewById(R.id.rv5Mascotas);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        listaMascotas.setLayoutManager(llm);
+
+        inicializarAdaptador();
+    }
+
+    public void inicializarAdaptador(){
+        MascotaAdaptador adaptador = new MascotaAdaptador(mascotas);
+        listaMascotas.setAdapter(adaptador);
+    }
+}
 
     }
 }
