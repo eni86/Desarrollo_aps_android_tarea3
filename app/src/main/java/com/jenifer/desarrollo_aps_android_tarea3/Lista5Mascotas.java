71532cbdaa16c0f01bc8fd9c87bd751e9ac9600e
eni86 <61356060+eni86@users.noreply.github.com>
2020-10-06 04:34:25 -0300

    .

--- app/src/main/java/com/jenifer/desarrollo_aps_android_tarea3/Lista5Mascotas.java
+++ app/src/main/java/com/jenifer/desarrollo_aps_android_tarea3/Lista5Mascotas.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toolbar;
 
@@ -12,11 +13,14 @@ import java.util.ArrayList;
 public class Lista5Mascotas extends AppCompatActivity {
 
     @Override
+    private ArrayList <Mascota> mascotas = new ArrayList<Mascota>();
+    private RecyclerView listaMascotas;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista5_mascotas);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        private ArrayList <Mascota> mascotas = new ArrayList<Mascota>();
+
 
         mascotas.add(new Mascota(R.drawable.cute_dog_headshot, "Toby",false, 15));
         mascotas.add(new Mascota(R.drawable.perro2, "Hugo",true, 16));
@@ -24,17 +28,22 @@ public class Lista5Mascotas extends AppCompatActivity {
         mascotas.add(new Mascota(R.drawable.perro4, "Luis",true, 17));
         mascotas.add(new Mascota(R.drawable.perro5, "Alvin",false, 20));
 
-        private RecyclerView listaMascotas;
         listaMascotas = (RecyclerView) findViewById(R.id.rv5Mascotas);
 
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         listaMascotas.setLayoutManager(llm);
+        inicializarAdaptador();
 
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
 
+       }
+    private void inicializarAdaptador(){
         MascotaAdaptador adaptador = new MascotaAdaptador(mascotas);
+        listaMascotas.setAdapter(adaptador); MascotaAdaptador adaptador = new MascotaAdaptador(mascotas);
         listaMascotas.setAdapter(adaptador);
-
     }
 
 
--- app/src/main/java/com/jenifer/desarrollo_aps_android_tarea3/MainActivity.java
+++ app/src/main/java/com/jenifer/desarrollo_aps_android_tarea3/MainActivity.java
@@ -4,20 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private ArrayList <Mascota> mascotas = new ArrayList<Mascota>();
+    private RecyclerView listaMascotas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        listaMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        private ArrayList <Mascota> mascotas = new ArrayList<Mascota>();
+
 
         mascotas.add(new Mascota(R.drawable.cute_dog_headshot, "Toby",false, 15));
         mascotas.add(new Mascota(R.drawable.perro2, "Hugo",true, 16));
@@ -26,18 +31,24 @@ public class MainActivity extends AppCompatActivity {
         mascotas.add(new Mascota(R.drawable.perro5, "Alvin",false, 20));
         mascotas.add(new Mascota(R.drawable.perro6, "Simon",true, 12));
         mascotas.add(new Mascota(R.drawable.perro7, "Theodore",true, 10));
-        private RecyclerView listaMascotas;
 
-        listaMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
+
+
 
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         listaMascotas.setLayoutManager(llm);
+        inicializarAdaptador();
 
+        Intent i = new Intent(this, Lista5Mascotas.class);
+        startActivity(i);
+        finish();
+
+    }
+    private void inicializarAdaptador(){
         MascotaAdaptador adaptador = new MascotaAdaptador(mascotas);
+        listaMascotas.setAdapter(adaptador); MascotaAdaptador adaptador = new MascotaAdaptador(mascotas);
         listaMascotas.setAdapter(adaptador);
-
-
     }
 
 
